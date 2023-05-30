b6f2533d51dde7d4d9e09259cd7d9b00655eac60
Lewis Parmenter <lewis.o.parmenter@gmail.com>
2023-05-30 15:50:05 +1000

    Initial commit and setup host builder.

---
+++ Custodian/Core/Services/CustodianService.cs
@@ -0,0 +1,26 @@
+﻿using Custodian.Core.Services;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Custodian.Core.Services
+{
+    internal class CustodianService : IHostedService
+    {
+        private ILogger _logger;
+
+        public CustodianService(ILogger<CustodianService> logger) 
+        { 
+            _logger = logger;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Started CustodianService.");
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Stopped CustodianService.");
+        }
+    }
+}
---
+++ Custodian/Program.cs
@@ -0,0 +1,24 @@
+﻿using Custodian.Core.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Custodian
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            IHost host = Host.CreateDefaultBuilder(args).ConfigureLogging(logging =>
+            {
+                logging.AddConsole();
+            }).ConfigureServices(services =>
+            {
+                services.AddHostedService<CustodianService>();
+            }).Build();
+
+            await host.RunAsync();
+            await host.WaitForShutdownAsync();
+        }
+    }
+}
